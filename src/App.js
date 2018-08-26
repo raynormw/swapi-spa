faac5bc1dc2352852db58f8b02133e7533f2300c
Tirta Wirya Putra <tirtawiryaputra@yahoo.com>
2018-08-26 21:56:26 +0700

    :hammer: Change to absolute path

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import MainHeader from "components/MainHeader";
 
 class App extends Component {
   render() {
-    return (<div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <h1 className="App-title">Welcome to React</h1>
-      </header>
-      <p className="App-intro">
-        To get started, edit
-        <code>src/App.js</code>
-        and save to reload.
-      </p>
-      <div class="container">
-        <div class="row">
-          <div class="col-sm">
-            One of three columns
-          </div>
-          <div class="col-sm">
-            One of three columns
-          </div>
-          <div class="col-sm">
-            One of three columns
+    return (
+      <div class="App">
+        <MainHeader />
+        <div className="container">
+          <div class="row">
+            <div class="col-sm test">One of three columns</div>
+            <div class="col-sm test">One of three columns</div>
+            <div class="col-sm test">One of three columns</div>
           </div>
         </div>
       </div>
-    </div>);
+    );
   }
 }
 
---
+++ src/components/MainHeader.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class MainHeader extends Component {
+  render() {
+    return (
+      <div className="container header">
+        <h3>Luke Skywalker</h3>
+        <div className="container content">
+          <div className="row">asd</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MainHeader;
