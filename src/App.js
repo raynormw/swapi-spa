5a317423301df90b4f099eb6dd45c8138bcf5da8
Tirta Wirya Putra <tirtawiryaputra@yahoo.com>
2018-08-27 04:23:49 +0700

    :wrench: Lastname formatting

--- src/App.js
+++ src/App.js
@@ -36,8 +36,8 @@ class App extends Component {
         <MainHeader data={data} />
         <div className="container">
           <div className="row">
-            <Content />
-            <div className="col-4">One of three columns</div>
+            <Content data={data} />
+            <div className="col-12 col-lg-4">One of three columns</div>
           </div>
         </div>
       </div>
--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
+import { shape } from "prop-types";
 
 import CardContent from "components/Content/CardContent";
 
 class MainContent extends Component {
+  static propTypes = {
+    data: shape({}).isRequired
+  };
+
   render() {
+    const { data } = this.props;
+    const split = data.name.split(" ");
+    const lastName = split[split.length - 1];
+
     return (
-      <div className="col-8 content">
+      <div className="col-12 col-lg-8 content">
         <div className="row justify-content-between mb-3">
-          <div className="col-4 actor-name">Skywalker</div>
+          <div className="col-8 actor-name">{lastName} Movie's</div>
           <div className="col-4 link">See More</div>
         </div>
         <CardContent />
